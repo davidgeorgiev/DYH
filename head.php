df30c8b32293db7d3f6f2426f8bfb0311008be5a
fixed-author <fixed-email>
2015-07-02 17:00:54 +0300

    My first commit
    Adding some main files

---
+++ head.php
@@ -0,0 +1,7 @@
+﻿<?php
+	echo '<head><meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script></head>';
+?>

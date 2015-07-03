60111f92bae88888a70e3f9eef1f09603d852f30
davidgeorgiev <david_georgiev@mail.bg>
2015-07-04 00:35:07 +0300

    Change one href

--- acc_added.php
+++ acc_added.php
@@ -48,7 +48,7 @@ include "config.php";
 			
 			mysql_close($dbLink);
 			echo '<h1>Поздравления, '.$name.'!</h1>';
-			echo '<p><a class="btn btn-primary btn-lg" href="home.php" role="button">Начало</a></p></div>';
+			echo '<p><a class="btn btn-primary btn-lg" href="index.php" role="button">Начало</a></p></div>';
 			
 			echo '<div class="alert alert-success" role="alert">Вашият акаунт беше създаден успешно!</div>';
 		}

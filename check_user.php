f18358283c407f19e3b540ed30fc07bc5e9766fa
davidgeorgiev <david_georgiev@mail.bg>
2015-07-04 22:59:53 +0300

    Fixing user check

--- check_user.php
+++ check_user.php
@@ -1,16 +1,18 @@
 <?php
 	session_start();
+	include "config.php";
 	$password = $_POST["psw"];
 	$username = $_POST["name"];
 	
-	$_SESSION['psw'] = $password;
-	$_SESSION['name'] = $username;
+	
 
 	$SQL = "SELECT COUNT(user.Name) FROM user WHERE user.Name = '".$username."' AND user.Password = '".$password."'";
 	$result = mysql_query($SQL);
 	$row = mysql_fetch_array($result);
 	
-	if ($row > 0) {
+	if ($row[0] > 0) {
+		$_SESSION['psw'] = $password;
+		$_SESSION['name'] = $username;
 		header('Location: home.php') and exit;
 	} else {
 		header('Location: notreg.php') and exit;

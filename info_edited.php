d9ebb246b0d0634bdd9f12d7faaea97f63ac8f87
davidgeorgiev <david_georgiev@mail.bg>
2015-07-05 22:38:12 +0300

    Adding ability to edit info

---
+++ info_edited.php
@@ -0,0 +1,63 @@
+﻿<?php
+	session_start();
+	
+?>
+<html>
+<?php 
+include "head.php";
+include "config.php";
+
+$password = $_SESSION['psw'];
+$username = $_SESSION['name'];
+include "CheckEditMode.php";
+$_SESSION['psw'] = $password;
+$_SESSION['name'] = $username;
+?>
+<body>
+
+<div class="container">
+<?php
+$_SESSION['page'] = "other";
+include "main_menu.php"; ?>
+	<div class="jumbotron">
+		<h1>Домашни</h1>
+		<p><?php echo $username?></p> 
+	</div>
+	<?php
+	if ($db_found && $EditMode == 1) {
+		$title = $_POST["title"];
+		$data = $_POST["data"];
+		$infoid = $_SESSION['infoid'];
+		
+		$SQL = "UPDATE otherinfo SET Title = '".$title."', Data = '".$data."' WHERE otherinfo.UID = ".$infoid;
+		$result = mysql_query($SQL);
+		
+		
+		mysql_close($dbLink);
+
+		echo '<div class="alert alert-success" role="alert"><a href="" class="alert-link"></a>Редакцията приключи успешно</div>';
+
+	}
+	else {
+
+		echo '<div class="alert alert-danger" role="alert"><a href="" class="alert-link"></a>Домашното не можа да се добави</div>';
+		mysql_close($dbLink);
+	}
+	?>
+  <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Информация за домашното</h3>
+  </div>
+  <div class="panel-body">
+    <?php 
+		echo '<p>Заглавие: '.$title.'</p>'; 
+		echo '<p>Описание: '.$data.'</p>'; 
+	?>
+  </div>
+</div>
+</div>
+
+
+</div>
+</body>
+</html>

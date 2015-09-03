0438bed99ba6b55aea3fae701e3edee14dc9982d
fixed-author <fixed-email>
2015-09-04 02:42:01 +0300

    Adding choose subject from list in add_hw.php and adding sorting of subjects by rank

--- add_hw.php
+++ add_hw.php
@@ -32,7 +32,24 @@
 <div class="container">
 <?php
 $_SESSION['page'] = "other";
-include "main_menu.php"; ?>
+include "main_menu.php"; 
+
+$SQL = "SELECT COUNT(usersubjectlist.UID) FROM usersubjectlist, user WHERE usersubjectlist.USERID = user.UID AND user.Name = '".$username."'";
+$result = mysql_query($SQL);
+$row = mysql_fetch_array($result);
+
+$theresnosubjects = 0;
+if ($row[0] <= 0) {
+	$theresnosubjects = 1;
+} else {
+	$SQL = "SELECT usersubjectlist.SUBJECTLISTID FROM usersubjectlist, user WHERE usersubjectlist.USERID = user.UID AND user.Name = '".$username."'";
+	//echo $SQL;
+	$result = mysql_query($SQL);
+	$row = mysql_fetch_array($result);
+	//echo "<p>".$row[0]."</p>";
+	$subject_ids_arr = explode(",", $row[0]);
+}
+?>
 	<div class="jumbotron">
 		<h1>Домашни</h1>
 		<p><?php echo $username?></p> 
@@ -51,10 +68,23 @@ include "main_menu.php"; ?>
 			<option value="1">Изпит</option>
 		</select>
     </div>
-    <div class="form-group">
-      <label for="text">Заглавие</label>
-      <input type="text" class="form-control" name="title" placeholder="Домашно - Математика">
-    </div>
+	  <?php
+		echo '<div class="form-group">';
+		if ($theresnosubjects == 1) {
+			echo '<label for="text">Нямате предмети създайте от опциите горе в менюто!</label>';
+		} else {
+			echo '<label for="text">Изберете от вашия списък с предмети!</label>';
+			echo '<select class="form-control" name="title">';
+			for ($i = 0;$i < sizeof($subject_ids_arr) - 1; $i++) {
+				$SQL = "SELECT subjects.Name, subjects.Rank FROM subjects WHERE subjects.UID = ".$subject_ids_arr[$i];
+				$result = mysql_query($SQL);
+				$row = mysql_fetch_array($result);
+				echo '<option value="'.$row[0].'">'.$row[0].'</option>';
+			}
+			echo '</select>';
+		}
+		echo '</div>';
+	  ?>
 	<div class="form-group">
       <label for="text">Описание</label>
       <textarea type="text" cols="50" rows="7" class="form-control" name="data" placeholder="Решете целия учебник"></textarea>
@@ -73,8 +103,14 @@ include "main_menu.php"; ?>
 		</select>
     </div>
 	<?php 
-		if ($EditMode == 1) {
-			echo '<button type="submit" class="btn btn-default">Submit</button>';
+		if (($EditMode == 1) && ($theresnosubjects == 0)) {
+			echo '<button type="submit" class="btn btn-default">Запази</button>';
+		} else {
+			if ($EditMode == 0) {
+				echo '<p>Не сте влезли в акаунта си!</p>';
+			} else {
+				echo '<p>Първо създайте предмети във вашия списък от опциите горе!</p>';
+			}
 		}
 	?>
 	</form>

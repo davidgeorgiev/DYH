f55ca82bf1d06259c1634e79510e8d3585924a42
davidgeorgiev <david_georgiev@mail.bg>
2015-09-04 07:06:34 +0300

    Fixing design and adding cool responsible date and time pickers

---
+++ check_width_and_send_to_add_hw.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+	$_SESSION['page'] = 'check_whidth';
+?>
+<script type="text/javascript">
+
+width = window.innerWidth;
+height = window.innerHeight;
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+if (width > 0 && height >0) {
+    window.location.href = "add_hw.php?width=" + width + "&height=" + height;
+} else 
+    exit();
+
+</script>

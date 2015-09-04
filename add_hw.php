d7e3ca4822fc115e858d3775bdd3b7adde875c5b
davidgeorgiev <david_georgiev@mail.bg>
2015-09-04 08:25:20 +0300

    Fixing edit mode and adding some design

--- add_hw.php
+++ add_hw.php
@@ -29,35 +29,8 @@
 	$_SESSION['psw'] = $password;
 	$_SESSION['name'] = $username;
 	//echo $EditMode;
+	include "some_external_phps/head_for_datepickers.php";
 ?>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-<link rel="stylesheet" type="text/css" href="Clean-jQuery-Date-Time-Picker-Plugin-datetimepicker/jquery.datetimepicker.css"/>
-<style type="text/css">
-
-.custom-date-style {
-	background-color: red !important;
-}
-
-</style>
-<script type="text/javascript">
-    var datefield=document.createElement("input")
-    datefield.setAttribute("type", "date")
-    if (datefield.type!="date"){ //if browser doesn't support input type="date", load files for jQuery UI Date Picker
-        document.write('<link href="jquery/jquery-ui.css" rel="stylesheet" type="text/css" />\n')
-        document.write('<script src="jquery/jquery.min.js"><\/script>\n')
-        document.write('<script src="jquery/jquery-ui.min.js"><\/script>\n') 
-    }
-</script>
- 
-<script>
-if (datefield.type!="date"){ //if browser doesn't support input type="date", initialize date picker widget:
-    jQuery(function($){ //on document.ready
-        $('#pickdate').datepicker();
-    })
-}
-</script>
-</head>
 <body>
 <div class="container">
 <?php

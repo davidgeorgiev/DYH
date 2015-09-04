4bac6e67c08cfb81a42f30811bf5c6c47633168d
davidgeorgiev <david_georgiev@mail.bg>
2015-09-04 11:11:34 +0300

    Adding title

--- head.php
+++ head.php
@@ -1,7 +1,8 @@
 ﻿<?php
 	echo '<head><meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">';
+  echo '<title>DYH - '.$_SESSION['name'].'</title>';
+  echo '<link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/my.css">
   <link rel="stylesheet" href="simple-sidebar-1.0.4/css/simple-sidebar.css">
   <script src="jquery.min.js"></script>

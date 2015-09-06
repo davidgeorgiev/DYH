8b043661667cdb7df7d97a3ceb96706d36e3204e
fixed-author <fixed-email>
2015-09-06 23:58:05 +0300

    Adding simple filter on home page

--- head.php
+++ head.php
@@ -12,6 +12,12 @@
   <script src="jquery.min.js"></script>
   <script src="themes/5/smooth-scroll.js"></script><script src="lightbox/js/jquery-1.11.0.min.js"></script><script src="lightbox/js/lightbox.min.js"></script><link href="lightbox/css/lightbox.css" rel="stylesheet" />
   <script src="js/bootstrap.min.js"></script></head>';
+	function ifLogged() {
+		$SQL = "SELECT COUNT(user.UID) FROM user WHERE user.Password = '".$_SESSION["psw"]."'";
+		$if_logged_result = mysql_query($SQL);
+		$if_logged = mysql_fetch_array($if_logged_result);
+		return $if_logged[0];
+	}
 	function Get_Logged_users_name() {
 		$SQL = "SELECT user.Name FROM user WHERE user.Password = '".$_SESSION["psw"]."'";
 		$logged_user_name_is = mysql_query($SQL);

5b4d60e51bcf79a5efb1bfd70de41a81c01f8a33
fixed-author <fixed-email>
2015-09-06 23:58:05 +0300

    Adding simple filter on home page

--- history.php
+++ history.php
@@ -6,7 +6,7 @@
 
 	$current_page_is = 'history';
 	//$current_page_is = 'home';
-	
+	$button_to_render = "";
 	$the_end_of_query = 'ORDER BY homeworks.Date DESC';
 	//$the_end_of_query = "AND homeworks.Date >= '\".date(\"Y-m-d\").\" 00:00:00' ORDER BY homeworks.Date ASC";
 	include "main_page.php";

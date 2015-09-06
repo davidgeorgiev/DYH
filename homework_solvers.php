28dadcb395c23ea811b6bae6fbb82b4d472434bb
davidgeorgiev <david_georgiev@mail.bg>
2015-09-06 22:00:19 +0300

    Fixing design of homework_solvers.php

--- homework_solvers.php
+++ homework_solvers.php
@@ -1,4 +1,14 @@
-﻿<?php
+﻿<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="vertical-timeline/css/reset.css">
+	<link rel="stylesheet" href="vertical-timeline/css/style.css">
+	<script src="vertical-timeline/js/modernizr.js"></script>
+</head>
+<?php
 	session_start();
 	echo '<html lang="en" class="no-js">';
 	include "head.php";
@@ -22,22 +32,36 @@
 	$SQL = "SELECT COUNT(solvedhomeworks.UID) FROM user, solvedhomeworks WHERE solvedhomeworks.USERID = user.UID AND solvedhomeworks.HWID = ".$_GET["hwid"];
 	$result = mysql_query($SQL);
 	$solvers_counter = mysql_fetch_array($result);
-	$SQL = "SELECT user.Name FROM user, solvedhomeworks WHERE solvedhomeworks.USERID = user.UID AND solvedhomeworks.HWID = ".$_GET["hwid"];
+	$SQL = "SELECT user.Name, solvedhomeworks.Date FROM user, solvedhomeworks WHERE solvedhomeworks.USERID = user.UID AND solvedhomeworks.HWID = ".$_GET["hwid"]." ORDER BY Date DESC";
 	//echo $SQL;
 	//print_r($solvers_counter);
 	$result = mysql_query($SQL);
 	include "Convert_data_from_solvedhomeworks_to_sentence.php";
+	echo '<section id="cd-timeline" class="cd-container">';
 	while ($solvers_names = mysql_fetch_array($result)){
+		echo '<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="vertical-timeline/img/cd-icon-picture.svg" alt="Picture">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">';
 		//print_r($solvers_names);
 		if ($solvers_counter[0] <= 0){
 			echo 'Никой не е решил още това домашно :(';
 		} else {
-			echo $solvers_names[0];
+			echo "<h2>".$solvers_names[0]."</h2>";
+			//echo $solvers_names[1];
 			$sentence = ConvertDataFromSolvedHomewokrsToSentence($solvers_names[0],$_GET["hwid"])[0];
 			$percents = ConvertDataFromSolvedHomewokrsToSentence($solvers_names[0],$_GET["hwid"])[1];
+			$date = ConvertDataFromSolvedHomewokrsToSentence($solvers_names[0],$_GET["hwid"])[2];
 			include "some_external_phps/show_solved_info.php";
+			echo "</div> <!-- cd-timeline-content -->";
+			echo "</div> <!-- cd-timeline-block -->";
 		}
 	}
+	echo "</section> <!-- cd-timeline -->";
+	echo '<script src="vertical-timeline/jquery.min.js"></script>
+	<script src="vertical-timeline/js/main.js"></script> <!-- Resource jQuery -->';
 ?>
 
 </div>

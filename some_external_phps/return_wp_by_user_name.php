5506d35353446644e38178def30d1fc8c626267b
davidgeorgiev <david_georgiev@mail.bg>
2015-09-08 20:07:41 +0300

    Removing redundancies in curriculum and fixing design

--- some_external_phps/return_wp_by_user_name.php
+++ some_external_phps/return_wp_by_user_name.php
@@ -28,14 +28,20 @@
 			for ($i=1; $i<=9; $i++){
 				
 				
-				$SQL = "SELECT class.time, class.subject, class.info FROM class, day, weeks, twoweeks, uw, user WHERE user.UID = uw.UserID AND twoweeks.UID = uw.TwoWeeksID AND twoweeks.".$eoweek." = weeks.UID AND day.UID = weeks.".$WeekDay."ID AND class.UID = day.class".$i."ID AND user.Name = '".$myusername."' ORDER BY twoweeks.UID DESC";
+				$SQL = "SELECT class.time, class.subjectid, class.info FROM class, day, weeks, twoweeks, uw, user WHERE user.UID = uw.UserID AND twoweeks.UID = uw.TwoWeeksID AND twoweeks.".$eoweek." = weeks.UID AND day.UID = weeks.".$WeekDay."ID AND class.UID = day.class".$i."ID AND user.Name = '".$myusername."' ORDER BY twoweeks.UID DESC";
 				//echo $SQL;
 				
 				$result3 = mysql_query($SQL);
 				$row3 = mysql_fetch_array($result3);
 				
-				$MyDayArray[$i]["Time"] = $row3[0];
-				$MyDayArray[$i]["Subject"] = $row3[1];
+				$MyDayArray[$i]["Time"] = strtotime($row3[0]);
+				$MyDayArray[$i]["Time"] = date('H:i',$MyDayArray[$i]["Time"]);
+				
+				$SQL = "SELECT subjects.Name FROM subjects WHERE subjects.UID = ".$row3[1];
+				$SubjectNameResult = mysql_query($SQL);
+				$MySubjectName = mysql_fetch_array($SubjectNameResult);
+				
+				$MyDayArray[$i]["Subject"] = $MySubjectName[0];
 				$MyDayArray[$i]["Info"] = $row3[2];
 				
 				

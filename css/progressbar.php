a8ad35d1f3b6b6295b3b4872d46faada42f3ea2b
davidgeorgiev <david_georgiev@mail.bg>
2015-09-10 11:56:35 +0300

    Removing redundancies from comments and adding design

---
+++ css/progressbar.php
@@ -0,0 +1,16 @@
+<style>
+#progressbar {
+	background-color: #837d7c;
+	border-radius: 3px; /* (height of inner div) / 2 + padding */
+	padding: 3px;
+}
+
+#progressbar > div {
+	background-color: #d2c9c6;
+	width: 40%; /* Adjust with JavaScript */
+	height: 20px;
+	border-radius: 3px;
+	text-align:center;font-size:16px;
+	font-weight: normal;
+}
+</style>

2feda015ff4eeee989c0633d860fe7f9314b9988
fixed-author <fixed-email>
2015-09-11 18:18:19 +0300

    Adding legend buttons for chart on main page

--- homeworks_time_chart.php
+++ homeworks_time_chart.php
@@ -109,9 +109,9 @@
 		$PrevMonth = 0;
 		for ($counter = 1; $counter <= $numberOfWeeks; $counter++){
 			
-			
-			$strDateFrom = MakeStartAndFinalDates($week_number, $year)[0];
-			$strDateTo = MakeStartAndFinalDates($week_number, $year)[1];
+			$StartAndFinelDates = MakeStartAndFinalDates($week_number, $year);
+			$strDateFrom = $StartAndFinelDates[0];
+			$strDateTo = $StartAndFinelDates[1];
 			
 			$PrevMonth = PrintAChart($counter, $username, $strDateFrom, $strDateTo, $PrevMonth, $EditMode);
 			
@@ -138,39 +138,9 @@
 include "main_menu.php"; 
 echo '<div id = "my_page" style = "background: rgba(243, 243, 243, 0.4);">';
 
-echo '<div><div class="dropdown" style = "float:left;padding-right:10px;">
-	<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style = "width:60px;height:46px;">
-	<span class="glyphicon glyphicon-wrench"></span>
-	</button>
-	<ul class="dropdown-menu" aria-labelledby="dropdownMenu2">';
 	
-	for ($counter = 1; $counter <= 15; $counter++) {
-		if ($counter == 1){
-			$MyWord = " седмица";
-		} else {
-			$MyWord = " седмици";
-		}
-		echo '<li><a href="check_width_and_send_to.php?user='.$username.'&page=homeworks_time_chart&weeknum='.$_GET["weeknum"].'&numofweeks='.$counter.'">Покажи '.$counter.$MyWord.'</a></li>';
-	}
-	
-	echo '</div><div style = "text-align:center;border:1px solid #c8ccc1;border-radius: 5px;padding: 4.5px;color: #243746;background-color: white;font-size:24;font-family:Arial	;font-weight: bold;">Графики на задачите (показани '.$_GET["numofweeks"].' седмици)</div></div>';
-	echo '
-	<div class="btn-group btn-group-justified" role="group" style = "width:100%;margin-bottom:30px;">
-	<div class="btn-group" role="group">
-	<button type="button" class="btn btn-default" style = "background: #86cf4b;border-color: #837d7c;border-width:3px;">Домашни</button>
-	</div>
-	<div class="btn-group" role="group">
-	<button type="button" class="btn btn-default" style = "background: #4ba8cf;border-color: #837d7c;border-width:3px;">Изпити</button>
-	</div>
-	<div class="btn-group" role="group">
-	<button type="button" class="btn btn-default" style = "background: #dd8043;border-color: #837d7c;border-width:3px;">Други</button>
-	</div>
-	<div class="btn-group" role="group">
-	<button type="button" class="btn btn-default" style = "color:white;background: #673e7a;border-color: #837d7c;border-width:3px;">Решени от вас</button>
-	</div>
-	</div>
-	';
-	
+	include "some_external_phps/LegendButtonsForChart.php";
+	PrintChartHeader($username, $_GET["weeknum"], 1, "");
 	
 	$SQL = "SELECT DISTINCT COUNT(user.Name) FROM user WHERE user.Name = '".$username."'";
 	$result4 = mysql_query($SQL);
